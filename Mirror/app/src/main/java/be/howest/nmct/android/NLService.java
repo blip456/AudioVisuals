92972e533182708e5793371eabdedbf7032403a8
blip456 <yoranbroodcooren@gmail.com>
2015-01-17 18:48:06 +0100

    App update to fixed IP + working carousel

--- Mirror/app/src/main/java/be/howest/nmct/android/NLService.java
+++ Mirror/app/src/main/java/be/howest/nmct/android/NLService.java
@@ -29,7 +29,7 @@ public class NLService extends NotificationListenerService {
 
     private String TAG = this.getClass().getSimpleName();
     private NLServiceReceiver nlservicereciver;
-    private String baseAPIurl = "http://192.168.2.205:3000/";
+    private String baseAPIurl = "http://192.168.0.145:3000/";
     public  List<String> arrImportantNotifications;
 
     @Override
